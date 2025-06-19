ccdecac38be67fd76a572bebfa0701e52b5d66f9
Aviral Mishra <avi.avimishra.com@gmail.com>
2025-06-19 07:34:36 +0000

    container config done

--- 01_basics/01_variables.js
+++ 01_basics/01_variables.js
@@ -2,5 +2,9 @@ const accountId = 1234445
 let accountEmail = "h@gmail.com"
 accountPassword = 123453
 accountCity = "Bengluru"
+/*
+Prefer not to use var..
+beacause of issue in block scope and functional scope..
+*/
 
 console.table([accountId,accountEmail,accountCity,accountPassword])
