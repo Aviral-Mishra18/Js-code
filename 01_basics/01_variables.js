475c982ed3aab4b66207002f5399edf96030db65
Aviral Mishra <avi.avimishra.com@gmail.com>
2025-06-19 07:30:35 +0000

    container config done

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,6 @@
+const accountId = 1234445
+let accountEmail = "h@gmail.com"
+accountPassword = 123453
+accountCity = "Bengluru"
+
+console.table([accountId,accountEmail,accountCity,accountPassword])
