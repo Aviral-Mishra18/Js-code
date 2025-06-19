0b280a61bbc963b46dabde65db5924d3b3e58e37
Aviral Mishra <avi.avimishra.com@gmail.com>
2025-06-19 08:25:29 +0000

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,25 @@
+//treat all js code as newer version..
+"use strict";
+
+// alert("hello")we are using node.js,not browser..
+
+console.log(3
+    +
+    3)//code readibility should be high..
+
+console.log("Aviral")
+
+let name = "Aviral"//string datatypes
+let age = 18//variable datatypes
+
+//number => 2 to power 53
+//bigint
+//string =>""
+//boolean => true/false
+//null => stanalone value
+//undefined =>
+// symbol=> unique
+
+
+console.log(typeof null);//null is a object..
+console.log(typeof undefined);// undefine returns undefine..
