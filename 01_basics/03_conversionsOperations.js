04405a1af6a1edfa595ca85c8974b8daa7d34947
Aviral Mishra <avi.avimishra.com@gmail.com>
2025-06-19 09:16:16 +0000

    conversionsOperations

---
+++ 01_basics/03_conversionsOperations.js
@@ -0,0 +1,26 @@
+let score = "33"
+
+//console.log(typeof score);
+//console.log(typeof (score));
+
+let valueInNumber = Number(score)
+//console.log(typeof valueInNumber);
+
+//"33" =>ceasily converted in 33..
+//"33abc"=>it gives value NaN..(Not a Number)
+// true = convert in 1
+
+let isLoggedIn = 1
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn);
+
+// 1=>true; 0=>false
+//""=>false//if we have empty string it automatically gives false..
+//"hitesh"=>true if there is string its gives true...
+
+let someNumber = 33
+
+let stringNumber = String(somenumber)
+console.log(stringNumber);
+console.log(stringNumber);
+
